9068fedf6ecadc73d6fc574a313fdab99d736c3c
Swapneel <swd543@gmail.com>
2019-10-19 03:04:58 +0200

    add type

---
+++ src/game_state_type.rs
@@ -0,0 +1,21 @@
+use crate::game_state_type::GameState::{BLACK, WHITE};
+
+pub type GameStateType = i8;
+
+#[derive(Debug, Clone, Copy, Hash, PartialEq)]
+pub enum GameState {
+    WHITE=8,
+    BLACK=7,
+    NONE=0,
+    INVALID=9
+}
+
+impl GameState {
+    pub fn from_game(value: GameStateType) -> GameState {
+        match value {
+            8 => WHITE,
+            7 => BLACK,
+            _ => {panic!("Invalid game state provided => {}!",value)}
+        }
+    }
+}
